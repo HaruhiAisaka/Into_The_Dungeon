b1379eb2e7481b0b90467341ac9fd5010d58f13e
Liam Riley <lrr42@cornell.edu>
2020-07-15 13:20:06 -1000

    Player Movement and Attack Down Animated

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,22 +6,30 @@ public class Player : MonoBehaviour
 {
     [Header("Player Movement")]
     public float speed = 1f;
-    public Transform player;
+
     public int playerHealth;
 
-    [SerializeField] Rigidbody2D myRigidBody2D;
+    public BoxCollider2D attackHitBox;
+
+    private Rigidbody2D myRigidBody2D;
+
+    private Animator myAnimator;
 
     // Start is called before the first frame update
     void Start()
     {
+        attackHitBox.enabled = false;
         myRigidBody2D = this.GetComponent<Rigidbody2D>();
+        myAnimator = this.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
         Move();
+        if(Input.GetButtonDown("Attack1")){
+            Attack();
+        }
     }
     
     // Move horizontally: left if `a` or left-button is pressed, right if `d` or right-button is pressed
@@ -30,5 +38,22 @@ public class Player : MonoBehaviour
         float deltaX = Input.GetAxisRaw("Horizontal") * speed;
         float deltaY = Input.GetAxisRaw("Vertical") * speed;
         myRigidBody2D.velocity = new Vector2(deltaX, deltaY);
+    }
+
+    private void Attack(){
+        attackHitBox.transform.position = 
+            new Vector3(attackHitBox.transform.position.x, 
+            attackHitBox.transform.position.y - 1, 
+            attackHitBox.transform.position.z);
+        myAnimator.SetTrigger("attack");
+        attackHitBox.enabled = true;
+    }
+
+    private void AttackEnd(){
+        attackHitBox.transform.position = 
+            new Vector3(attackHitBox.transform.position.x, 
+            attackHitBox.transform.position.y + 1, 
+            attackHitBox.transform.position.z);
+        attackHitBox.enabled = false;
     }
 }
