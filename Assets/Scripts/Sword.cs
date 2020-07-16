c8776a8c42aba654f5b653799e63f3cde2f40d38
Quinn Theobald <qjt3@cornell.edu>
2020-07-15 22:37:12 -0400

    sword script

---
+++ Assets/Scripts/Sword.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // When it hits an enemy
+    void OnTriggerEnter2D(Collider2D Collision) {
+        if (Collision.gameObject.tag == "enemy") {
+            Debug.Log("HIT");
+            // TO IMPLEMENT: damage the enemy
+            // TO IMPLEMENT: knock the enemy back
+        }
+    }
+}
