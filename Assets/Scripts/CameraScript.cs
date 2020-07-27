f40ea5cad26e1e8b63a8dc577b9d68aa58d6d549
bobingbobs <53265559+bobingbobs@users.noreply.github.com>
2020-07-26 20:27:43 -1000

    Revert "Items, keys, doors can  be closed, item inventory, item database"

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public Transform camera;
+    public GameObject player;
+    public Vector2 playerLocation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerLocation = player.GetComponent<Transform>().position;
+        camera.position = new Vector3(playerLocation.x, playerLocation.y, -1);
+    }
+}
