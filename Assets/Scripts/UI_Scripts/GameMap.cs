141d8426036eb9db6a20e37a3a57dbf5734d228d
Phillip O'Reggio <pnoreggio@gmail.com>
2020-08-06 17:26:30 -0400

    Setup tilemap for the main map

---
+++ Assets/Scripts/UI_Scripts/GameMap.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.UI;
+
+public class GameMap : MonoBehaviour
+{
+
+    private Tilemap map;
+
+    // Start is called before the first frame update
+    void Start() {
+        map = this.GetComponent<Tilemap>();
+    }
+
+}
