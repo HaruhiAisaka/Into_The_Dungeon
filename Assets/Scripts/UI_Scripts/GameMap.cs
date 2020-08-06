ad1788bcd059b9a54fef020dad4a087b8173bf0a
Phillip O'Reggio <pnoreggio@gmail.com>
2020-08-06 19:56:27 -0400

    Work on simple tilebased map

--- Assets/Scripts/UI_Scripts/GameMap.cs
+++ Assets/Scripts/UI_Scripts/GameMap.cs
@@ -7,11 +7,37 @@ using UnityEngine.UI;
 public class GameMap : MonoBehaviour
 {
 
-    private Tilemap map;
+    [SerializeField] Tilemap map;
+    [SerializeField] Tile tile;
 
     // Start is called before the first frame update
     void Start() {
-        map = this.GetComponent<Tilemap>();
+        // testing drawing
+        Debug.Log("Adding to tilemap?");
+        map.SetTile(new Vector3Int(1 , 1 , 0), tile);
+        map.SetTile(new Vector3Int(2 , 2 , 0), tile);
+        map.SetTile(new Vector3Int(3 , 3 , 0), tile);
+        map.SetTile(new Vector3Int(4 , 4 , 0), tile);
+        map.SetTile(new Vector3Int(5 , 5 , 0), tile);
+        map.SetTile(new Vector3Int(6 , 6 , 0), tile);
+        map.SetTile(new Vector3Int(7 , 7 , 0), tile);
+        map.SetTile(new Vector3Int(8 , 8 , 0), tile);
+        map.SetTile(new Vector3Int(9 , 9 , 0), tile);
+        map.SetTile(new Vector3Int(10, 10, 0), tile);
+        map.SetTile(new Vector3Int(11, 11, 0), tile);
+        map.SetTile(new Vector3Int(12, 12, 0), tile);
+        map.SetTile(new Vector3Int(13, 13, 0), tile);
+        map.SetTile(new Vector3Int(14, 14, 0), tile);
+        map.SetTile(new Vector3Int(15, 15, 0), tile);
+        map.SetTile(new Vector3Int(16, 16, 0), tile);
+        map.SetTile(new Vector3Int(17, 17, 0), tile);
+        map.SetTile(new Vector3Int(18, 18, 0), tile);
+        map.SetTile(new Vector3Int(19, 19, 0), tile);
+
+        map.SetTile(new Vector3Int(100, 100, 0), tile);
+        map.SetTile(new Vector3Int(200, 200, 0), tile);
+        map.SetTile(new Vector3Int(300, 300, 0), tile);
+        map.SetTile(new Vector3Int(400, 400, 0), tile);
     }
 
 }
