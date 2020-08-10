08f4bcb197818203a9df66cb6e882a9e221931b5
Liam Riley <lrr42@cornell.edu>
2020-08-10 12:57:29 -1000

    Menu Selector

--- Assets/Scripts/Menu_Scripts/MenuSelector.cs
+++ Assets/Scripts/Menu_Scripts/MenuSelector.cs
@@ -1,18 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuSelector : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] RectTransform selector;
+    [SerializeField] Button[] buttons;
+
+    private void Update() {
+        UpdateSelectorPosition();
+        Vector3[] corners = new Vector3[4];
+        buttons[0].GetComponent<RectTransform>().GetLocalCorners(corners);
+        Debug.Log(corners[0]);
+        Debug.Log(corners[1]);
+        Debug.Log(corners[2]);
+        Debug.Log(corners[3]);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void UpdateSelectorPosition(){
+
+    }
+
+    private void MoveSelectorToButton(Button button){
+        RectTransform buttonRect = button.GetComponent<RectTransform>();
     }
 }
