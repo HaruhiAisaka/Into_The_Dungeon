9148104aaf30e47ddd016b06d11cb0f8b650ade0
Liam Riley <lrr42@cornell.edu>
2020-08-11 21:18:11 -1000

    Title Screen Completed

---
+++ Assets/Scripts/UI_Scripts/Menu_Scripts/MenuOption.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MenuOption : MonoBehaviour
+{
+    [SerializeField] protected MenuSelector menuSelector;
+
+    // Procedure that is executed when OnHighlighted is called on Any Option.
+    public void OnHighlighted(){
+        menuSelector.SetCurrentOption(this);
+        OnHighlightedUnique();
+    }
+
+    // Procedures that are unique to the Menu Option that inherets this class.
+    protected abstract void OnHighlightedUnique();
+    public abstract void  OnSelect();
+}
